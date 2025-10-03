a53ec680b76d5c56e9680a0274b6204934af16b3
Yash Narula <yashnarula@Yashs-MacBook-Air-3.local>
2025-10-03 15:28:47 +0530

    Updating State Correctly Based On Old State

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,13 +5,14 @@ export default function Player({name , symbol}) {
     const [isEditing , setIsEditing] = useState(false);
     
     function handleEditClick(){
-       setIsEditing(true);
+       //setIsEditing(!isEditing);
+       setIsEditing((editing) => !editing)
     }
     
     let playerName =  <span className="player-name">{name}</span>
 
     if(isEditing){
-       playerName = <input type="text" required/>
+       playerName = <input type="text" required value={name}/>
     }
     return (
         <li>
@@ -19,7 +20,7 @@ export default function Player({name , symbol}) {
          {playerName}
          <span className="player-symbol">{symbol}</span>
         </span>
-        <button onClick = {handleEditClick}>Edit</button>
+        <button onClick = {handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
       </li>
     );
 }
